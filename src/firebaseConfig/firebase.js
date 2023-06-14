1764ef9f52a3f7fa58502cb762b8ddb6ac114d97
davidgrinberg17 <davidgrinberg17@hotmail.com>
2023-06-14 12:43:26 -0300

    Update firebase.js

--- src/firebaseConfig/firebase.js
+++ src/firebaseConfig/firebase.js
@@ -6,12 +6,7 @@ import { getFirestore } from "@firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBph3sD80aG0dnc2q1IVldKIvgbY93HJQg",
-    authDomain: "cac-react-23307-deg.firebaseapp.com",
-    projectId: "cac-react-23307-deg",
-    storageBucket: "cac-react-23307-deg.appspot.com",
-    messagingSenderId: "1025194032827",
-    appId: "1:1025194032827:web:70cbf49de97b20e251b39e"
+    
   };
 
 // Initialize Firebase
